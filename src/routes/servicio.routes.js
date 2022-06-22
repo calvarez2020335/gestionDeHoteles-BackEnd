a1d8c2c162b1e490af335abb6c747cb0678d00a4
Carlos Adolfo Alvarez Crúz <calvarez-2020335@kinal.edu.gt>
2022-06-22 11:35:00 -0600

    Crud completo de servicio

--- src/routes/servicio.routes.js
+++ src/routes/servicio.routes.js
@@ -7,5 +7,9 @@ const md_roles = require('../middlewares/roles');
 const api = express.Router();
 
 api.post('/registrarServicio/:idHotel', [md_autenticacion.Auth, md_roles.verHotelAdmin], servicioController.registrarServicio);
+api.get('/verServicios/:idHotel', [md_autenticacion.Auth], servicioController.verServicios);
+api.get('/verServiciosId/:idServicio', md_autenticacion.Auth, servicioController.verServiciosId);
+api.put('/editarServicio/:idServicio', [md_autenticacion.Auth, md_roles.verHotelAdmin], servicioController.editarServicio);
+api.delete('/eliminarServicio/:idServicio', [md_autenticacion.Auth, md_roles.verHotelAdmin], servicioController.eliminarServicio);
 
 module.exports = api;
