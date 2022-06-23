7e477f09e4129c3310f1adcd0d9fe5f17c53a48d
Carlos Adolfo Alvarez Crúz <calvarez-2020335@kinal.edu.gt>
2022-06-22 18:44:11 -0600

    Facturas

--- src/controllers/factura.controller.js
+++ src/controllers/factura.controller.js
@@ -1,16 +1,25 @@
 const Factura = require('../models/factura.model');
 const Usuario = require('../models/usuario.model');
+const Servicio = require('../models/servicio.model');
+const GastosServicios = require('../models/gastosServicios.model');
 
-
-function CreaFactura (req , res) {
-	idUser = req.params.idUser;
+function confirmarFactura (req , res) {
+	var idFactura = req.params.idFactu;
 	var parametro = req.body;
 	var facturaModelo = new Factura();
     
 
+	// GastosServicios.find()
+
+	Factura.findOneAndUpdate ({_id: idFactura } , {$pull: { servicios: { nombreServicios : null , precio: 0}}} , {new: true} , (err, facturaActualzada) =>{
+		if (err) return res.status(500).send({ mensaje: 'error en la peticion del eliminar el carrito' });
+		if (!facturaActualzada) return res.status(500).send({ mensaje: 'error al eliminar el producto al carrito' });
+
+	});
+    
 
 }
 
 module.exports = {
-	CreaFactura
+	confirmarFactura
 };
