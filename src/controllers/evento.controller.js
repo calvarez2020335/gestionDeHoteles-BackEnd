ea892786d3b572e1b66603b4de8fabdac89b92e6
Carlos Adolfo Alvarez Crúz <calvarez-2020335@kinal.edu.gt>
2022-06-29 20:35:04 -0600

    Solocionar agregar evento

--- src/controllers/evento.controller.js
+++ src/controllers/evento.controller.js
@@ -3,58 +3,38 @@ const Hoteles = require('../models/hotel.model');
 
 function crearEvento(req, res) {
 	const parametro = req.body;
+	const idHotel = req.params.idhotel;
 	const eventoModel = new Evento();
 
-	if (
-		(parametro.Nombre,
-		parametro.Descripcion,
-		parametro.hotel,
-		parametro.tipoEvento)
-	) {
-		Hoteles.findOne(
-			{
-				Nombre: { $regex: parametro.hotel, $options: 'i' },
-				Dueno: req.user.sub,
-			},
-			(err, hotelEncontrado) => {
-				if (!hotelEncontrado)
-					return res.status(500).send({ mensaje: 'Hotel no encontrado' });
-				console.log(hotelEncontrado);
-				Evento.find(
-					{
-						Nombre: { $regex: parametro.Nombre, $options: 'i' },
-						adminHotel: req.user.sub,
-						hotel: hotelEncontrado._id,
-					},
-					(err, eventoEncontrado) => {
-						if (eventoEncontrado.length > 0)
-							return res
-								.status(500)
-								.send({ mensaje: 'Este evento ya a sido creado' });
-
-						eventoModel.Nombre = parametro.Nombre;
-						(eventoModel.tipoEvento = parametro.tipoEvento),
-						(eventoModel.Descripcion = parametro.Descripcion);
-						eventoModel.hotel = hotelEncontrado._id;
-						eventoModel.adminHotel = req.user.sub;
-						if (req.file) {
-							const { filename } = req.file;
-							eventoModel.setImgUrl(filename);
-						}
-						eventoModel.save((err, eventoGuardado) => {
-							if (err)
-								return res
-									.status(500)
-									.send({ mensaje: 'Error en la peticion de guardar evento' });
-							if (!eventoGuardado)
-								return res
-									.status(500)
-									.send({ mensaje: 'Error al guardar evento' });
-							return res.status(200).send({ eventoGuardado: eventoGuardado });
-						});
-					}
-				);
+	if (parametro.Nombre, parametro.Descripcion, parametro.tipoEvento) {
+		Hoteles.findOne({hotel: idHotel, Dueno: req.user.sub,},(err, hotelEncontrado) => {
+			if (!hotelEncontrado) return res.status(500).send({ mensaje: 'Hotel no encontrado' });
+			Evento.find({ Nombre: { $regex: parametro.Nombre, $options: 'i' }, adminHotel: req.user.sub, hotel: idHotel},(err, eventoEncontrado) => {
+				if (eventoEncontrado.length > 0)
+					return res.status(500).send({ mensaje: 'Este evento ya a sido creado' });
+				eventoModel.Nombre = parametro.Nombre;
+				eventoModel.tipoEvento = parametro.tipoEvento,
+				eventoModel.Descripcion = parametro.Descripcion;
+				eventoModel.hotel = idHotel;
+				eventoModel.adminHotel = req.user.sub;
+				if (req.file) {
+					const { filename } = req.file;
+					eventoModel.setImgUrl(filename);
+				}
+				eventoModel.save((err, eventoGuardado) => {
+					if (err)
+						return res
+							.status(500)
+							.send({ mensaje: 'Error en la peticion de guardar evento' });
+					if (!eventoGuardado)
+						return res
+							.status(500)
+							.send({ mensaje: 'Error al guardar evento' });
+					return res.status(200).send({ eventoGuardado: eventoGuardado });
+				});
 			}
+			);
+		}
 		);
 	} else {
 		return res
@@ -74,7 +54,6 @@ function editarEventos(req, res) {
 	}
 	parametros.image = eventosModel.imgUrl;
 	Evento.findById({ _id: idEvento }, (err, eventoEncontrado) => {
-		console.log(eventoEncontrado);
 		if (eventoEncontrado.adminHotel != req.user.sub)
 			return res
 				.status(500)
--- src/routes/evento.routes.js
+++ src/routes/evento.routes.js
@@ -6,7 +6,7 @@ const api = express.Router();
 const md_autenticacion = require('../middlewares/autenticacion');
 const md_roles = require('../middlewares/roles');
 
-api.post('/crearEvento', [md_autenticacion.Auth, md_roles.verHotelAdmin, upload.single('image')], eventoController.crearEvento);
+api.post('/crearEvento/:idhotel', [md_autenticacion.Auth, md_roles.verHotelAdmin, upload.single('image')], eventoController.crearEvento);
 api.put('/editarEvento/:idEvento', [md_autenticacion.Auth, md_roles.verHotelAdmin], eventoController.editarEventos);
 api.delete('/eliminarEvento/:idEvento', [md_autenticacion.Auth, md_roles.verHotelAdmin], eventoController.eliminarEventos);
 api.get('/verEventos/:idHotel', md_autenticacion.Auth, eventoController.verEventos);
