72e342fb01c1aaba63a77e8c46bcf458b7661447
Carlos Adolfo Alvarez Crúz <calvarez-2020335@kinal.edu.gt>
2022-07-11 16:04:03 -0600

    Conexion base de datos en la nube

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const {crearAdminInicio} = require('./src/controllers/usuario.controller');
 
 mongoose.Promise = global.Promise;
 
-//mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@api-rest.h9udywp.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority
+//
 
-mongoose.connect('mongodb://localhost:27017/Hoteles', { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
+mongoose.connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@api-rest.h9udywp.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
 
 	app.listen(process.env.PORT || 3000, function () {		
 		crearAdminInicio();
